c9f2178f0779b2b272f75c0dc22720a79a18a5ca
Nam2108 <namnhhe161947@fpt.edu.vn>
2024-07-05 23:00:29 +0700

    check login
    check login

--- MyAssignment/src/java/dal/UsersDBContext.java
+++ MyAssignment/src/java/dal/UsersDBContext.java
@@ -4,10 +4,84 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import moder.Lecturers;
+import moder.Users;
+
 /**
  *
  * @author nam
  */
-public class UsersDBContext {
+public class UsersDBContext extends DBContext<Users>{
+    public Users getUserByUsernamePassword(String username, String password) {
+        PreparedStatement stm =null;
+        Users user = null;
+        try {
+            String sql = "SELECT u.username,u.displayname,l.lid,l.lname\n"
+                    + "FROM users u LEFT JOIN users_lecturers ul ON ul.username = u.username AND ul.active = 1\n"
+                    + "						LEFT JOIN lecturers l ON ul.lid = l.lid\n"
+                    + "						WHERE u.username = ? AND u.[password] = ?";
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            stm.setString(2, password);
+            ResultSet rs = stm.executeQuery();
+            if(rs.next())
+            {
+                user = new Users();
+                user.setDisplayname(rs.getString("displayname"));
+                user.setUsername(username);
+                int lid = rs.getInt("lid");
+                if(lid!=0)
+                {
+                   Lecturers lecturer = new Lecturers();
+                   lecturer.setId(lid);
+                   lecturer.setName(rs.getString("lname"));
+                   user.setLecturer(lecturer);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally
+        {
+            try {
+                stm.close();
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsersDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return user;
+    }
+    
+    @Override
+    public void insert(Users model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Users model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Users model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Users get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Users> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
